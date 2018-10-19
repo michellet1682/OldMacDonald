d04e99667ee3525cdadd69f3f9900945e015ec22
michellet1682 <42549577+michellet1682@users.noreply.github.com>
2018-10-19 09:53:33 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,12 +1,12 @@
-void setup()
+class NamedCow extends Cow
 {
-  Cow c = new Cow("cow", "moo");   
-  System.out.println(c.getType() + " goes " + c.getSound());
-  Chick k = new Chick("chick", "bawk","cheep");
-  System.out.println(k.getType() + " goes " + k.getSound());
-  Pig p = new Pig("pig", "oink");
-  System.out.println(p.getType() + " goes " + p.getSound());
-  
-  Farm d = new Farm();
-  d.animalSounds();
+  private String myCowname;
+  public NamedCow(String type, String name, String sound){
+    myType = type;
+    myCowname = name;
+    mySound = sound;
+  }
+  public String getName(){
+    return myCowname;
+  }
 }
